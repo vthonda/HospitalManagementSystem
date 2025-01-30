db02eff16b72457e6597cb3b553d33e5eaf60f96
Vaishnavi Thonda <104980483+vthonda@users.noreply.github.com>
2025-01-30 11:41:52 +0530

    Update settings.py

--- hospitalmanagement/settings.py
+++ hospitalmanagement/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'hpbv()ep00boce&o0w7z1h)st148(*m@6@-rk$nn)(n9ojj4c0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['hospitalmanagementsystem-x4xf.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['hospitalmanagementsystem-s231.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
